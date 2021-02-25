6b229f0b2c12a1aa6b5e8e29d456ec4ed84533dc
Joshua Patton <jgp9k9@umsystem.edu>
2021-02-25 09:56:33 -0600

    Add mathFcns.py ioFcns.py and geometry.py and imports.

---
+++ tools/RAiDER/ioFcns.py
@@ -0,0 +1,20 @@
+""" General function related to file I/O """
+import h5py
+import importlib
+import os
+import pyproj
+import re
+import requests
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+from datetime import datetime
+from osgeo import gdal, osr
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+from RAiDER.geometry import checkLOS
+
+gdal.UseExceptions()
